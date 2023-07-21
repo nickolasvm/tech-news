8bcdef1d0fcee61a18f17ebbf9a14b1ef3362688
nickolasvm <nickolas64vm@gmail.com>
2023-07-21 19:18:48 -0300

    feat: Created function scrape_news to get the necessary info from a news page

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,18 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import re
 
 HEADER = {"user-agent": "Fake user-agent"}
 
 
-def fetch(url: str) -> str:
+def fetch(url: str) -> str | None:
     time.sleep(1)
 
     try:
         response = requests.get(url, headers=HEADER, timeout=2)
         if response.status_code == 200:
-            print(type(response.text))
-            print(type(url))
             return response.text
 
     except requests.ReadTimeout:
@@ -28,7 +27,7 @@ def scrape_updates(html_content: str) -> list[str]:
     return links
 
 
-def scrape_next_page_link(html_content: str) -> str:
+def scrape_next_page_link(html_content: str) -> str | None:
     soup = BeautifulSoup(html_content, "html.parser")
 
     next_link = soup.find("a", {"class": "next"})
@@ -39,10 +38,42 @@ def scrape_next_page_link(html_content: str) -> str:
         return None
 
 
-# Requisito 4
-def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+def extract_digit_from_string(string: str) -> int:
+    digits = re.findall(r"\d", string)
+    result = int("".join(digits))
+    return result
+
+
+def clean_text(text: str) -> str:
+    cleaned_text = text.replace("\xa0", "")
+    if cleaned_text[-1] == " ":
+        cleaned_text = cleaned_text[:-1]
+    return cleaned_text
+
+
+def scrape_news(html_content: str) -> dict:
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    url = soup.find("link", {"rel": "canonical"})["href"]
+    title = clean_text(soup.find("h1", {"class": "entry-title"}).text)
+    timestamp = soup.find("li", {"class": "meta-date"}).text
+    writer = soup.find("span", {"class": "author"}).a.text
+    reading_time = extract_digit_from_string(
+        soup.find("li", {"class": "meta-reading-time"}).text
+    )
+    summary = clean_text(soup.find("div", {"class": "entry-content"}).p.text)
+    category_tag = soup.find("a", {"class": "category-style"})
+    category = category_tag.find("span", {"class": "label"}).text
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
