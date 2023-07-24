013543c55f0a5acbd46af0f1b649772ef4590d23
nickolasvm <nickolas64vm@gmail.com>
2023-07-24 16:16:35 -0300

    feat: created function top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,24 @@
-# Requisito 10
+from tech_news.database import db
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    categories = list(db.news.find({}, {"_id": False, "category": True}))
+
+    for i, c in enumerate(categories):
+        categories[i]["count"] = db.news.find(
+            {"category": c["category"]}
+        ).count()
+
+    #  sort alphabetically
+    categories = sorted(categories, key=lambda x: x["category"])
+    #  sort by count
+    categories = sorted(categories, key=lambda x: x["count"], reverse=True)
+
+    result = []
+    seen = set()
+    for c in categories:
+        if c["category"] not in seen and len(result) < 5:
+            seen.add(c["category"])
+            result.append(c["category"])
+
+    return result
