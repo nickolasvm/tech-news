9ec09992f7517a3e7dc51433b525fb9e25c4dbd2
nickolasvm <nickolas64vm@gmail.com>
2023-07-24 14:38:48 -0300

    feat: Created function search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 from tech_news.database import search_news
 
 
@@ -12,10 +13,27 @@ def search_by_title(title: str) -> list[tuple]:
     return result
 
 
-# Requisito 8
-def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+def convert_date_format(date: str) -> str:
+    input_format = "%Y-%m-%d"
+    output_format = "%d/%m/%Y"
+
+    parsed_date = datetime.strptime(date, input_format)
+    converted_date = parsed_date.strftime(output_format)
+
+    return converted_date
+
+
+def search_by_date(date: str) -> list[tuple]:
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+
+        result = []
+        for el in search_news({"timestamp": convert_date_format(date)}):
+            result.append((el["title"], el["url"]))
+
+        return result
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
